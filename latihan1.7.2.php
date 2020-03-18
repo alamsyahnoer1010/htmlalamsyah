6e45f376a60ff6073f5c00d9188b365b3b81fe1e
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-18 20:22:30 +0700

    Create latihan1.7.2.php

---
+++ latihan1.7.2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Latihan 2</title>
+	</head>
+	<body>
+	<h2>Menambahkan data</h2>
+	<br/>
+	<a href="latihan1.7.1.php"><<< kembali</a>
+	<h3>Tambah data mahasiswa</h3>
+	<form method="post" action="latihan1.7.3.php">
+		<table>
+			<tr>
+				<td>NIM</td>
+				<td><input type="number" name="nim"></input></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></input></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></input></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Simpan"></input></td>
+			</tr>
+		</table>
+	</form>
+	</body>
+</html>
