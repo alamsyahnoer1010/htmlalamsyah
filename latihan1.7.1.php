9fc8f44c1acb8164beea7adecf9a84d4bcb9b6cb
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-18 20:04:45 +0700

    Create latihan1.7.1.php

---
+++ latihan1.7.1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Latihan 1</title>
+	</head>
+	<body>
+		<h2>Menampilkan data mahasiswa</h2>
+		<table border="1">
+			<tr>
+				<th>No.</th>
+				<th>NIM</th>
+				<th>Nama</th>
+				<th>Alamat</th>
+			</tr>
+			<?php
+			include 'koneksi.php';
+			$no = 1;
+			$data = mysqli_query($koneksi,"select * from mahasiswa");
+			while ($d = mysqli_fetch_array($data)) {
+				?>
+				<tr>
+					<td><?php echo $no++; ?></td>
+					<td><?php echo $d['nim']; ?></td>
+					<td><?php echo $d['nama']; ?></td>
+					<td><?php echo $d['alamat']; ?></td>
+				</tr>
+				<?php
+			}
+			?>
+		</table>
+		<a href="latihan1.7.2.php"> + tambah data mahasiswa
+	</body>
+</html>
