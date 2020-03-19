a79bd84af9812f49c685b87349c939a5911d549a
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:49:03 +0700

    Create latihan1.8.7

---
+++ latihan1.8.7.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Seleksi</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			$a = 10; // membuat variabel dan memberi nilai variabel
+			$b = 5; // membuat variabel dan memberi nilai variabel
+			if ($a > $b){ // untuk melakukan operasi percabangan if
+				echo 'a lebih besar dari b'; // menampilkan kalimat a lebih besar dari b
+			}
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
