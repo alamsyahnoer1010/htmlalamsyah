ee055f1dd2a6ec7f70eeffd10a2bf32f8286c919
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 14:11:56 +0700

    Create latihan1.8.13

---
+++ latihan1.8.13.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Loop for</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			for ($i=0; $i < 10; $i++) { // peryataan perulangan for
+				echo $i; // mencetak nilai variabel $i
+			}
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
