b7c0495b40b716b4f8a349d961f7129077a67302
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:39:05 +0700

    Create latihan1.8.2

---
+++ latihan1.8.2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo Variabel</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			// Deklarasi dan inisialisasi
+			$bil = 5; // membuat variabel dan memberi nilai variabel
+			echo $bil; // mencetak variabel $bil
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
