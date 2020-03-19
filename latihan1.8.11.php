287e591589f952023074e76f2f4450bdbd673907
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 14:03:14 +0700

    Create latihan1.8.11

---
+++ latihan1.8.11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Loop while</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			$i = 0; // membuat variabel dan memberi nilai variabel
+			while ($i < 10) { // peryataan perulangan while
+				echo $i; // mencetak nilai variabel $i
+				// Inkremen counter
+				$i++;
+			}
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
