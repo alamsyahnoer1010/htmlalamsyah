9769fa04e9b869e22a72469b94f2193b4ed6f980
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:31:59 +0700

    Create latihan1.8

---
+++ latihan1.8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			echo "Kode PHP disini"; // menampilkan kalimat Kode PHP disini
+			// ...
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+		<p>Dokumen HTML</p> <!-- Mencetak kalimat Dokumen HTML di paragraf baru -->
+		<?php // sintak untuk mengawali bahasa php
+			echo "Kode PHP di sini"; // menampilkan kalimat Kode PHP di sini
+			// ...
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
