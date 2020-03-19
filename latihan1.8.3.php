92fc6d59d70065f670e95fd30b0786b697e80be1
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:41:42 +0700

    Create latihan1.8.3

---
+++ latihan1.8.3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo Variabel</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			// Deklarasi dan inisialisasi
+			$bil = 5; // membuat variabel dan memberi nilai variabel
+			// Dumping informasi mengenai variabel
+			var_dump($bil); // mencetak nilai pada variabel beserta tipe datanya
+			print_r($bil); // mencetak nilai pada variabel. fungsinya hampir sama dengan echo
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
