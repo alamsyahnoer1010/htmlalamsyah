da2263f4da175d325c2d3e8c55481407a6d097c7
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:49:59 +0700

    Create latihan1.8.8

---
+++ latihan1.8.8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Seleksi if - else</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			$a = 10; // membuat variabel dan memberi nilai variabel
+			$b = 5; // membuat variabel dan memberi nilai variabel
+			if ($a > $b) { // untuk melakukan operasi percabangan if
+				echo 'a lebih besar dari b'; // menampilkan kalimat a lebih besar dari b
+			} else { // jika pernyataan 1 salah maka beralih ke sintak else
+				echo 'a TIDAK lebih besar dari b'; // menampilkan kalimat a TIDAK lebih besar dari b
+			}
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
