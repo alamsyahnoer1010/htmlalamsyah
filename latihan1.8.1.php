72d011aca5fa820529a2d938067a449c7f06eb7a
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:37:08 +0700

    Create latihan1.8.1

---
+++ latihan1.8.1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo</title>
+	</head>
+	<body>
+		<p>Kode <?php echo "PHP";?> di HTML</p> <!-- Mencetak kalimat Kode PHP di HTML pada paragraf baru. Yang mana pada kata PHP di deklarasikan dalam sintaks php -->
+	</body>
+</html>
