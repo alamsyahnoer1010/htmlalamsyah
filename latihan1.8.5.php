52374b21116e5a2c864bf017044535d60aa8df28
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-19 13:44:07 +0700

    Create latihan1.8.5

---
+++ latihan1.8.5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cek Tipe</title>
+	</head>
+	<body>
+		<?php // sintak untuk mengawali bahasa php
+			$bil = 5; // membuat variabel dan memberi nilai variabel
+			var_dump(is_int($bil)); // menguji nilai pada variabel
+			// Output : bool (true)
+			$var = ""; // membuat variabel dan memberi nilai variabel
+			var_dump(is_string($var));
+			// Output : bool (true)
+		?> <!-- sintak untuk mengakhiri bahasa php -->
+	</body>
+</html>
