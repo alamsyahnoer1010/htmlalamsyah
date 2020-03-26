ec70af361f3f9ec59c580a4765a85de698ea329e
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-03-26 22:44:59 +0700

    Create latihan.1.9.php

---
+++ latihan1.9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Metode GET</title>
+	</head>
+	<body>
+		<form action="<?php $_SERVER['PHP_SELF'];?>" method="get"> Nama
+			<input type="text" name="nama"/> <br />
+			<input type="submit" value="OK" />
+		</form>
+		<?php
+			if (isset($_GET['nama'])) {
+				echo 'Hello,' . $_GET['nama'];
+			}
+		?>
+	</body>
+</html>
