22b8bc9c4eb9aa44901010f44cfe9f717e881827
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-05-08 09:14:30 +0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,35 @@
+<div id="produk">
+	<table align="center" height="490px">
+		<tr>
+			<td width="500" align="left"><img src="1272157194.png" width="400px" height="400px"></td>
+			<td width="300">
+				<font face="mileadilan" size="10">Form Register</font>
+				<hr size="2">
+				<form method="post" action="tambah.php">
+					<table align="left">
+						<tr>
+							<td>Nama</td>
+							<td>:</td>
+							<td><input type="text" name="nama" placeholder="Masukkan Nama Anda"></input></td>
+						</tr>
+						<tr>
+							<td>Alamat</td>
+							<td>:</td>
+							<td><input type="text" name="alamat" placeholder="Masukkan Alamat Selengkap Mungkin"></input></td>
+						</tr>
+						<tr>
+							<td>Password</td>
+							<td>:</td>
+							<td><input type="password" name="password" placeholder="Masukkan Password Anda"></input></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td><input type="submit" value="Daftar"></input></td>
+						</tr>
+					</table>
+				</form>
+			</td>
+		</tr>
+	</table>
+</div>
