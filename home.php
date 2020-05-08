3fb3923186c1807db003b2cc8b6137692320c857
Alamsyah Nur Muslimin <56432506+alamsyahnoer1010@users.noreply.github.com>
2020-05-08 09:12:04 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,63 @@
+<div id="header">
+	<font color="white" face="The Brandy" size="18">
+		<center>Selamat Datang di ANM Bakery</center>
+	</font>
+	<font color="white" size="5" face="Comic Sans MS">
+		<center>Anm Bakery merupakan toko kue dan oleh - oleh khas dari Gunungkidul.
+			<br>ANM Bakery menyediakan berbagai macam kue mulai dari kue bolu,
+			<br>brownies, dan kue lainnya. Jika anda berkunjung ke Gunungkidul,
+			<br>rasanya belum afdal kalau belum beli oleh - oleh dari ANM Bakery.
+			<br>ANM Bakery mengutamakan rasa, bahan, dan kebersihan produk,
+			<br>demi untuk menunjang standar kualitas produk yang kami jaga sekian lama ini.
+			<br>Dan untuk pelanggan ANM Bakery.
+		</center>
+	</font>
+	<br>
+	<center>
+		<font color="white" size="5" face="Comic Sans MS">
+		<br> Selamat Menikmati produk oleh - oleh khas ANM Bakery, dan
+		<br> Selamat Berbelanja di ANM Bakery
+		</font>
+	</center>
+</div>
+<div id="isi">
+	<center>
+		<font size="12" color="white">Isi harimu dengan sepotong kue setiap hari</font>
+	</center>
+</div>
+<table cellspacing="25" align="center">
+	<tr>
+		<td>
+			<a href="index.php?page=kue1" style="text-decoration: none">
+			<img src="Summer-Sangria-Cake-4.jpg" width="250px" height="250px"><font face ="Arial Rounded MT Bold" size="3" color="black">
+			<br> Summer Sangria Cake
+			<br> Rp. 200.000
+			</font>
+			</a>
+		</td>
+		<td>
+			<a href="index.php?page=kue2" style="text-decoration: none">
+			<img src="pastry1.jpg" width="250px" height="250px"><font face ="Arial Rounded MT Bold" size="3" color="black">
+			<br> Berry Fruit Pastries
+			<br> Rp. 150.000
+			</font>
+			</a>
+		</td>
+		<td>
+			<a href="index.php?page=kue3" style="text-decoration: none">
+			<img src="Raspberry-Cream-Cheese-Pastries.jpg" width="250px" height="250px"><font face ="Arial Rounded MT Bold" size="3" color="black">
+			<br> Raspberry Cream Cheese Pastries
+			<br> Rp. 150.000
+			</font>
+			</a>
+		</td>
+		<td>
+			<a href="index.php?page=kue4" style="text-decoration: none">
+			<img src="easter-nest-cake.jpg" width="250px" height="250px"><font face ="Arial Rounded MT Bold" size="3" color="black">
+			<br> Easter Nest Cake
+			<br> Rp. 150.000
+			</font>
+			</a>
+		</td>
+	</tr>
+</table>
